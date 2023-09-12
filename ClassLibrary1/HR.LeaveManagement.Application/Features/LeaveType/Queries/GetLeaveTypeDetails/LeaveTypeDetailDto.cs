0c23748a83d65b66fb34e0d64159bd388465e2be
Hajili Jeyhun <JeyhunpH@code.local>
2023-09-12 18:15:35 +0400

    leaveTyper deatil added

---
+++ ClassLibrary1/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/LeaveTypeDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public class LeaveTypeDetailDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DefaultDays { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ ClassLibrary1/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeDetailProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveTypeDetailProfile:Profile
+{
+	public LeaveTypeDetailProfile()
+	{
+		CreateMap<LeaveType,LeaveTypeDetailDto>().ReverseMap();
+	}
+}
