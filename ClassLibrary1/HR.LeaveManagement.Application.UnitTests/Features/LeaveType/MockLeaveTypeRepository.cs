d93df79c6bdbe7a5dc27d9394b1298e4800ea0e0
jekaH1 <ceyhun100203@gmail.com>
2023-10-05 15:46:57 +0400

    mocks

---
+++ ClassLibrary1/HR.LeaveManagement.Application.UnitTests/Features/LeaveType/MockLeaveTypeRepository.cs
@@ -0,0 +1,46 @@
+﻿using HR.LeaveManagement.Application.Contracts.Percistance;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Features.LeaveType;
+
+public class MockLeaveTypeRepository
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypeMockRepository()
+    {
+        var leaveTypes = new List<Domain.LeaveType>()
+        {
+            new Domain.LeaveType
+            {
+                Id = 1,
+                Name = "Foo",
+                DefaultDays = 1,
+            },
+            new Domain.LeaveType
+            {
+                Id = 2,
+                Name = "Boo",
+                DefaultDays = 30,
+            },
+            new Domain.LeaveType
+            {
+                Id = 3,
+                Name = "Doo",
+                DefaultDays = 13,
+            },
+        };
+
+        var mockRepo = new Mock<ILeaveTypeRepository>();
+
+        mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(leaveTypes);
+
+        mockRepo.Setup(r => r.AddAsync(It.IsAny<Domain.LeaveType>()))
+            .Returns((Domain.LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType);
+                return Task.CompletedTask;
+            });
+
+        return mockRepo;
+    }
+}
---
+++ ClassLibrary1/HR.LeaveManagement.Application/Exceptions/NotFoundException - Copy.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.LeaveManagement.Application.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message,ValidationResult validationResult) : base(message)
+    {
+
+        
+
+    }
+
+    public IDictionary<string, string[]> ValidationsErrors { get; set; }
+}
